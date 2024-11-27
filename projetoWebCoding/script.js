822d62e647a4ff927d331ac3ed6d4809498a6973
Luiz Henrique <hluiz1505@gmail.com>
2024-11-26 21:40:08 -0300

    att header

--- projetoWebCoding/script.js
+++ projetoWebCoding/script.js
@@ -1,3 +1,22 @@
-document.getElementById("toggle-dark-mode").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
+
+// JS da Galeria
+const images = document.querySelectorAll('.image-container img');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        const star = image.parentElement.querySelector('.star');
+        star.classList.toggle('active');
+        // Aqui você pode adicionar a lógica para salvar os favoritos (localStorage, banco de dados, etc.)
+    });
 });
+  
+// Fim do JS da Galeria  
